6c71b314273f3bfe264e7923519ebb0ca0477e38
zaramgithub <zaramagu15@gmail.com>
2023-03-18 20:19:10 -0300

    install multer to upload a image-backup of images

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const multer = require('multer');
+const path = require('path');
 
-const router = express();
+const router = express.Router();
+const diskstorage = multer.diskStorage(
+    {
+        destination: path.join(__dirname,'../images'),
+        filename: (req,file,cb) =>{
+            cb(null,Date.now()+'-agustinamaraz-'+file.originalname)
+        }
+    }
+);
+
+const fileUpload = multer(
+    {
+        storage: diskstorage
+    }
+).single('image');
 
 router.get('/',(req,res)=>{
     res.send('Welcome to my gallery!');
+});
+
+router.post('/images/post',fileUpload,(req,res)=>{
+    console.log(req.file);
 });
 
 module.exports = router;
